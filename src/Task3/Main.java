36946e314b8ed2f63b17e81fd98b4ec540a106a3
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-11-14 18:20:56 +0600

    added 3 rounds logic; deleted useless comments and code

--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -1,26 +1,14 @@
 package Task3;
 
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        new Main().start();
-
-        // hero.attack(dragon);
-        // dragon.attack(hero);
-        // hero.attack(dragon);
-        // dragon.attack(hero);
-
-        // if (hero.isAlive()) {
-        // System.out.println("герой жив");
-        // }
-        // if (dragon.isAlive()) {
-        // System.out.println("дракон жив");
-        // }
-
+        new Main().start(1);
+        new Main().start(2);
+        new Main().start(3);
     }
 
-    public void start() {
+    public void start(int bigRound) {
+        System.out.println("Большой раунд " + bigRound + " начался");
         Hero hero = new Hero(1000, 100, 120, 250, 150);
         Dragon dragon = new Dragon(2000, 120, 150, 250);
         int round = 1;
@@ -28,7 +16,7 @@ public class Main {
             System.out.println("Round " + round);
             hero.attack(dragon);
             if (dragon.isAlive()) {
-                dragon.attack(hero);;
+                dragon.attack(hero);
             }
             round++;
             System.out.println("======================");
@@ -39,5 +27,12 @@ public class Main {
         } else {
             System.out.println("Dragon wins!");
         }
+        System.out.println("Большой раунд " + bigRound + " закночился");
+        System.out.println();
+        System.out.println();
+        System.out.println("============================================");
+        System.out.println();
+        System.out.println();
+
     }
 }
