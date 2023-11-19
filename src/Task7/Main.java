e501c62407225702823d164509983439ab9644fe
Mari-Darling <zaebabo4ka77@gmail.com>
2023-11-19 21:36:34 +0600

    fixed isFresch method

--- src/Task7/Main.java
+++ src/Task7/Main.java
@@ -2,13 +2,12 @@ package Task7;
 
 public class Main {
     public static void main(String[] args) {
-    Store store = new Store();
-    store.fillingTheStoreWithGoods();
+        Store store = new Store();
+        store.fillingTheStoreWithGoods();
         for (int i = 0; i < store.products.length; i++) {
             System.out.println(store.products[i].getTitle());
             System.out.println(store.products[i].isFresh());
             System.out.println("----------------------------------");
-
         }
     }
 }
--- src/Task7/Store.java
+++ src/Task7/Store.java
@@ -14,19 +14,19 @@ public class Store {
             int randomProduct = random.nextInt(5);
             switch (randomProduct) {
                 case 0:
-                    products[i] = new Milk("Молоко", numberOfDaysToSubtract, productionDate);
+                    products[i] = new Milk("Молоко", 60, productionDate);
                     break;
                 case 1:
-                    products[i] = new Salt("Соль", numberOfDaysToSubtract, productionDate);
+                    products[i] = new Salt("Соль", 1000, productionDate);// переделать на инф
                     break;
                 case 2:
-                    products[i] = new Fish("Рыба", numberOfDaysToSubtract, productionDate);
+                    products[i] = new Fish("Рыба", 20, productionDate);
                     break;
                 case 3:
-                    products[i] = new Corn("Кукуруза", numberOfDaysToSubtract, productionDate);
+                    products[i] = new Corn("Кукуруза", 150, productionDate);
                     break;
                 case 4:
-                    products[i] = new Stew("Тушёнка", numberOfDaysToSubtract, productionDate);
+                    products[i] = new Stew("Тушёнка", 180, productionDate);
                     break;
             }
         }
