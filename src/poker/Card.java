1d32b8d4d944e9627f8bab67e0dbe9d000d1d2c7
Mari-Darling <zaebabo4ka77@gmail.com>
2023-11-22 21:05:33 +0600

    added Deck class

--- src/poker/Card.java
+++ src/poker/Card.java
@@ -4,10 +4,20 @@ public  class Card {
 
     private Suit suit; // масть
     private String rank; // ранг  или значение карты
+    private  boolean isAvalible;
 
     public Card(Suit suit, String rank) {
         this.suit = suit;
         this.rank = rank;
+        this.isAvalible = true;
+    }
+
+    public boolean isAvalible() {
+        return isAvalible;
+    }
+
+    public void setAvalible(boolean available) {
+        isAvalible = available;
     }
 
     @Override
